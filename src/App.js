64e0f73eddefc020fe0a16e6c617abd72fa7e78d
BB <leebobae@LeeBobaeui-MacBook-Pro.local>
2020-11-02 20:24:58 +0900

    ad logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import logo from '../src/asset/image/MADE4_logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
