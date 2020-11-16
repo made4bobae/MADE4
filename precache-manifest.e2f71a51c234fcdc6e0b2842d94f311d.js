393b0b87a890a3741fa14b4dea286129465f0234
fredkeemhaus <fredkeemhaus@gmail.com>
2020-11-16 22:00:28 +0900

    Updates

---
+++ precache-manifest.e2f71a51c234fcdc6e0b2842d94f311d.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d3665c1d80272560c56871e5bafd57",
+    "url": "/made4/index.html"
+  },
+  {
+    "revision": "64915e2a1721a079a87d",
+    "url": "/made4/static/js/2.56880ca9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/made4/static/js/2.56880ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0835f4f4150d6c4fb75",
+    "url": "/made4/static/js/main.f6a5376b.chunk.js"
+  },
+  {
+    "revision": "c37b517c0d67e4a497e3",
+    "url": "/made4/static/js/runtime-main.3770eec4.js"
+  },
+  {
+    "revision": "5ede2bd62e67fac417acdebc7ca5ab74",
+    "url": "/made4/static/media/americano.5ede2bd6.png"
+  },
+  {
+    "revision": "59545e01e4b5046168f43e62d6604d1f",
+    "url": "/made4/static/media/cafe_latte.59545e01.png"
+  },
+  {
+    "revision": "8b13f3935e91d837b84b13d82bf67f9e",
+    "url": "/made4/static/media/cafe_mocha.8b13f393.png"
+  },
+  {
+    "revision": "fec92153964554ea2fc8b0fb7c107cfb",
+    "url": "/made4/static/media/caramelmacchiato.fec92153.png"
+  },
+  {
+    "revision": "23f83143fc0eff2fd271b994599de118",
+    "url": "/made4/static/media/double_chocolate_latte.23f83143.png"
+  },
+  {
+    "revision": "a3a11694e2a2f03b93089d6dce2637ca",
+    "url": "/made4/static/media/event_insureum.a3a11694.png"
+  },
+  {
+    "revision": "6a780a0c5042e66923a5352cc8e597bd",
+    "url": "/made4/static/media/event_main.6a780a0c.png"
+  },
+  {
+    "revision": "e6ab47b7b307ce2a680e8c624a83816c",
+    "url": "/made4/static/media/event_main_mobile.e6ab47b7.png"
+  },
+  {
+    "revision": "234d4cf6f17ba23db8a9f34a6952358b",
+    "url": "/made4/static/media/event_wallet.234d4cf6.png"
+  },
+  {
+    "revision": "0b0baeb13a319a86d41b9efaa72c2848",
+    "url": "/made4/static/media/google-play.0b0baeb1.png"
+  },
+  {
+    "revision": "5e62b6074ea300bca221741188f0cf1b",
+    "url": "/made4/static/media/home_item_01.5e62b607.png"
+  },
+  {
+    "revision": "d6078529094334a51367e03f59c022fb",
+    "url": "/made4/static/media/home_item_02.d6078529.png"
+  },
+  {
+    "revision": "498d368131c2b37d8f9ae5b9b2103f00",
+    "url": "/made4/static/media/home_item_03.498d3681.png"
+  },
+  {
+    "revision": "c0f69b27c832724a7fa85d618511cdd4",
+    "url": "/made4/static/media/home_main.c0f69b27.png"
+  },
+  {
+    "revision": "d1e1b1a764a9581be336e89d1f0cea55",
+    "url": "/made4/static/media/home_main_mobile.d1e1b1a7.png"
+  },
+  {
+    "revision": "937c4ed533111c98086e24d13681b42f",
+    "url": "/made4/static/media/home_mall.937c4ed5.png"
+  },
+  {
+    "revision": "234d4cf6f17ba23db8a9f34a6952358b",
+    "url": "/made4/static/media/home_wallet.234d4cf6.png"
+  },
+  {
+    "revision": "cca65ce448c15090698dadb064e50e05",
+    "url": "/made4/static/media/jeju_malcha_latte.cca65ce4.png"
+  },
+  {
+    "revision": "ce1180baaf47da51bba204b39b8576c3",
+    "url": "/made4/static/media/lemon_tea.ce1180ba.png"
+  },
+  {
+    "revision": "9c74a5cf1e1b023ef969bacbc4f27b97",
+    "url": "/made4/static/media/logo.9c74a5cf.png"
+  },
+  {
+    "revision": "48247461e2260115787b322e8fee4398",
+    "url": "/made4/static/media/made4Logo.48247461.png"
+  },
+  {
+    "revision": "f21927b6da08efbe4f70c867dde5d97f",
+    "url": "/made4/static/media/made4_main.f21927b6.png"
+  },
+  {
+    "revision": "3d1511d80d4baa5aab872b7640c8702c",
+    "url": "/made4/static/media/peanut_latte.3d1511d8.png"
+  },
+  {
+    "revision": "822f6aa33214ecc17d30e3357cb4976f",
+    "url": "/made4/static/media/peppermint.822f6aa3.png"
+  },
+  {
+    "revision": "4f3e134e7f886fafe0dc223f1703e07e",
+    "url": "/made4/static/media/premium_milk_tea.4f3e134e.png"
+  },
+  {
+    "revision": "ec04543d8741f48cf77eecc7baac50c3",
+    "url": "/made4/static/media/vinilla_latte.ec04543d.png"
+  }
+]);
